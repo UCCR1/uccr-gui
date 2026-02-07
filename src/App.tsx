e5a73d704130eb9bf92ee95cadb94f137f9a7acf
Alex Cutforth <alex@densem.net>
2026-02-08 09:28:29 +1300

    add formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [devices, setDevices] = useState<string[]>([]);
 
-  const [devices, setDevices] = useState<string[]>([]);
+    const [connected, setConnected] = useState(false);
 
-  const [connected, setConnected] = useState(false);
+    useEffect(() => {
+        invoke<string[]>("get_devices").then((devices) => setDevices(devices));
+    }, []);
 
-  useEffect(() => {
-    invoke<string[]>("get_devices").then(devices => setDevices(devices));
-  }, []);
+    return (
+        <main>
+            <h1>Welcome to UCCR GUI</h1>
+            {connected ? (
+                <h2>Conncted</h2>
+            ) : (
+                <>
+                    <h2>Devices:</h2>
 
-  return (
-    <main>
-      <h1>Welcome to UCCR GUI</h1>
-      {connected ? <h2>Conncted</h2> : <><h2>Devices:</h2>
-
-      {devices.map(device => <button onClick={() => invoke("connect", { port: device }).then(() => setConnected(true))} className="bg-slate-300 p-2 rounded-md hover:bg-slate-400 cursor-pointer">{device}</button>)}
-   </>} </main>
-  );
+                    {devices.map((device) => (
+                        <button
+                            key={device}
+                            type="button"
+                            onClick={() =>
+                                invoke("connect", { port: device }).then(() =>
+                                    setConnected(true),
+                                )
+                            }
+                            className="bg-slate-300 p-2 rounded-md hover:bg-slate-400 cursor-pointer"
+                        >
+                            {device}
+                        </button>
+                    ))}
+                </>
+            )}{" "}
+        </main>
+    );
 }
 
 export default App;
